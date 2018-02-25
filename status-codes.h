c4b34f1f875d36fc2b04dd2473dd12336e1b27ab
Russell Coight <32256825+OutbackMan@users.noreply.github.com>
2018-02-26 10:40:19 +1100

    Update status-codes.h

--- status-codes.h
+++ status-codes.h
@@ -1,9 +1,11 @@
 #ifndef __status_codes_h__
 #define __status_codes_h__
 
+#include <stddef.h>
+
 typedef enum status_codes {
-	RGAME_SUCCESS = 1,
-	RGAME_FAILURE = -1,
+	RGAME_FAILURE,
+	RGAME_SUCCESS
 } rgame_status_t;
 
 char* str_rgame_status(rgame_status_t status_code);
