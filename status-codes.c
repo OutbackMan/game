4558a9c7bb778020c1256ee8739f55a26581f018
Russell Coight <32256825+OutbackMan@users.noreply.github.com>
2018-02-26 09:36:00 +1100

    Update status-codes.c

--- status-codes.c
+++ status-codes.c
@@ -1,22 +1,27 @@
 #include "status-codes.h"
+#include <stddef.h>
+#include <stdbool.h>
 #include <string.h>
-
-#define STATUS_MESSAGE_BUFFER_SIZE ((unsigned int)100)
 
 const char* str_rgame_status(rgame_status_t status_code)
 {
-	char message_buffer[STATUS_MESSAGE_BUFFER_SIZE] = {0};
+	if (!is_valid_status_code(status_code)) {
+		return "Unknown Status Code\n";
+	} else {
+		const size_t MESSAGE_BUFFER_SIZE = 100;
+		char message_buffer[MESSAGE_BUFFER_SIZE] = {0};
+	
+		sscanf(&message_buffer, "(Status Code: %d) %s\n", status_code, status_code_strings[status_code]);
 
-	switch (status_code) {
-		case RGAME_SUCCESS:
-			sscanf_s(&message_buffer, "(Status Code: %d) RGame Success\n", RGAME_SUCCESS);  
-			break;
-		case RGAME_FAILURE:
-			sscanf_s(&message_buffer, "(Status Code: %d) RGame Failure\n", RGAME_FAILURE);  
-			break;
-		default:
-			sscanf_s(&message_buffer, "Unknown status code given\n");
+		return message_buffer;
 	}
+}
 
-	return message_buffer;
+static bool is_valid_status_code(rgame_status_t status_code)
+{
+	if (status_code < 0 || status_code >= status_code_lengths) {
+		return false;
+	} else {
+		return true;
+	}
 }
