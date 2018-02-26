7ce459ec097682c837e7d8275df68c64739101f2
Russell Coight <32256825+OutbackMan@users.noreply.github.com>
2018-02-26 11:00:05 +1100

    Update status-codes.c

--- status-codes.c
+++ status-codes.c
@@ -3,25 +3,37 @@
 #include <stdbool.h>
 #include <string.h>
 
-const char* str_rgame_status(rgame_status_t status_code)
-{
-	if (!is_valid_status_code(status_code)) {
-		return "Unknown Status Code\n";
-	} else {
-		const size_t MESSAGE_BUFFER_SIZE = 100;
-		char message_buffer[MESSAGE_BUFFER_SIZE] = {0};
-	
-		sscanf(&message_buffer, "(Status Code: %d) %s\n", status_code, status_code_strings[status_code]);
-
-		return message_buffer;
-	}
-}
-
 static bool is_valid_status_code(rgame_status_t status_code)
 {
-	if (status_code < 0 || status_code >= status_code_lengths) {
+	if (status_code < 0 || status_code >= NUMBER_OF_STATUS_CODES) {
 		return false;
 	} else {
 		return true;
 	}
 }
+
+static const char* lookup_status_code_string(rgame_status_t status_code)
+{
+	if (!is_valid_status_code(status_code)) {
+		return "Unknown Status Code";
+	}
+	
+	static const char* status_code_strings[NUMBER_OF_STATUS_CODES] = {
+		"RGame Failure",
+		"RGame Success",
+	};
+	
+	return status_code_strings[status_code];
+}
+
+const char* str_rgame_status(rgame_status_t status_code)
+{
+	const size_t MESSAGE_BUFFER_SIZE = 100;
+	char message_buffer[MESSAGE_BUFFER_SIZE] = {0};
+	
+	sscanf(&message_buffer, "(Status Code: %d) %s\n", status_code, lookup_status_code_string(status_code));
+
+	return message_buffer;
+}
+
+
