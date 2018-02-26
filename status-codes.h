216635125b96db826fc763953f0bc1ff0c4ba2a0
Russell Coight <32256825+OutbackMan@users.noreply.github.com>
2018-02-27 10:08:12 +1100

    Update status-codes.h

--- status-codes.h
+++ status-codes.h
@@ -12,7 +12,18 @@ typedef enum status_codes {
 static const size_t NUMBER_OF_STATUS_CODES = 3
 static const size_t MAX_STATUS_CODE_VALUE = 2;
 
-const rgame_status_t rgame_status(const rgame_status_t status_code, const char msg[restrict static 1], const char file_location[restrict static 1], 
+#if defined(WANT_LOGGING1)
+#define RGAME_STATUS(STATUS_CODE) \
+		rgame_status_log(1, STATUS_CODE, __FILE__, __func__, __LINE__)
+#elif defined(WANT_LOGGING2)
+#define RGAME_STATUS(STATUS_CODE) \
+		rgame_status_log(2, STATUS_CODE, __FILE__, __func__, __LINE__)
+#elif defined(WANT_LOGGING3)
+#define RGAME_STATUS(STATUS_CODE) \
+		rgame_status_log(3, STATUS_CODE, __FILE__, __func__, __LINE__)
+#endif
+
+const rgame_status_t rgame_status_log(const int log_level, const rgame_status_t status_code, const char msg[restrict static 1], const char file_location[restrict static 1], 
 							const char function_name[restrict static 1], const int line_number);
 
 const char* str_rgame_status(rgame_status_t status_code);
