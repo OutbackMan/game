953f15974e979e67903f1b1f296f9e0ecac08307
Russell Coight <32256825+OutbackMan@users.noreply.github.com>
2018-02-27 10:22:34 +1100

    Update status-codes.h

--- status-codes.h
+++ status-codes.h
@@ -2,6 +2,7 @@
 #define __status_codes_h__
 
 #include <stddef.h>
+#include <stdbool.h>
 
 typedef enum status_codes {
 	RGAME_ERROR,
@@ -11,6 +12,15 @@ typedef enum status_codes {
 
 static const size_t NUMBER_OF_STATUS_CODES = 3
 static const size_t MAX_STATUS_CODE_VALUE = 2;
+
+static inline bool is_valid_status_code(rgame_status_t status_code)
+{
+	if (status_code < 0 || status_code >= MAX_STATUS_CODE_VALUE) {
+		return false;
+	} else {
+		return true;
+	}
+}
 
 #if defined(WANT_LOGGING1)
 #define RGAME_STATUS(STATUS_CODE) \
@@ -23,7 +33,7 @@ static const size_t MAX_STATUS_CODE_VALUE = 2;
 		rgame_status_log(3, STATUS_CODE, __FILE__, __func__, __LINE__)
 #endif
 
-const rgame_status_t rgame_status_log(const int log_level, const rgame_status_t status_code, const char msg[restrict static 1], const char file_location[restrict static 1], 
+const rgame_status_t rgame_status_log(const int log_level, const rgame_status_t status_code, const char file_location[restrict static 1], 
 							const char function_name[restrict static 1], const int line_number);
 
 const char* str_rgame_status(rgame_status_t status_code);
