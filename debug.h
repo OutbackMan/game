5091f6062401af8fa61a492286ef76118743f0ba
Russell Coight <32256825+OutbackMan@users.noreply.github.com>
2018-02-26 13:57:41 +1100

    Update debug.h

--- debug.h
+++ debug.h
@@ -8,13 +8,22 @@
 
 // NDEBUG will stop asserts
 #ifndef NDEBUG
-#define debug_log(MSG) fprintf(stderr, "[RGAME_LOG]" MSG " (%s:%s:%d)\n",\
+#define rgame_log(MSG) fprintf(stderr, "[RGAME_LOG]" MSG " (%s:%s:%d).\n",\
                             __FILE__, __func__, __LINE__)
-#define debug_error(MSG) fprintf(stderr, "[RGAME_ERROR] (errno: %s) " MSG " (%s:%s:%d)\n",\
+#define rgame_error(MSG) fprintf(stderr, "[RGAME_ERROR] (errno: %s) " MSG " (%s:%s:%d).\n",\
                             clean_errno(), __FILE__, __func__, __LINE__)
 #else
 #define debug_log(MSG)
 #define debug_error(MSG)
 #endif
+
+inline static void rgame_sdl_error(const char message[restrict static 1])
+{
+    #ifndef NDEBUG
+        fprintf(stderr, "[RGAME_SDL_ERROR] %s.\n", message);
+    #else
+        return;
+    #endif
+}
 
 #endif
