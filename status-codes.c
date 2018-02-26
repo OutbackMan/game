3af5a402c06fbc6ba831a4db59c4c0eba13cbc51
Russell Coight <32256825+OutbackMan@users.noreply.github.com>
2018-02-27 10:21:23 +1100

    Update status-codes.c

--- status-codes.c
+++ status-codes.c
@@ -6,15 +6,6 @@
 #include <assert.h>
 #include <errno.h>
 
-static bool is_valid_status_code(rgame_status_t status_code)
-{
-	if (status_code < 0 || status_code >= MAX_STATUS_CODE_VALUE) {
-		return false;
-	} else {
-		return true;
-	}
-}
-
 static const char* lookup_status_code_string(rgame_status_t status_code)
 {
 	if (!is_valid_status_code(status_code)) {
@@ -35,20 +26,23 @@ static inline const char* clean_errno(void)
 	return (errno == 0 ? "None" : strerror(errno));
 }
 
-const rgame_status_t rgame_status(const rgame_status_t status_code, const char msg[restrict static 1], const char file_name[restrict static 1], 
+const rgame_status_t rgame_status_log(const int log_level, const rgame_status_t status_code, const char file_name[restrict static 1], 
 					const char function_name[restrict static 1], const int line_number)
 {
+	assert(is_valid_log_level(log_level));
 	assert(is_valid_status_code(status_code));
 	
-    #ifdef WANT_LOGGING1
-		fprintf(stderr, "%s (%s:%s:%d) %s (errno: %s).\n", lookup_status_code_string(status_code), 
-			file_name, function_name, line_number, msg, clean_errno());
-	#elif defined(WANT_LOGGING2) || defined(WANT_LOGGING3)
+	const char* log_message = (status_code == RGAME_SDL_ERROR ? SDL_GetError() : ""); 
+	
+    if (log_level == 1) {
+    	fprintf(stderr, "%s (%s:%s:%d) %s (errno: %s).\n", lookup_status_code_string(status_code), 
+			file_name, function_name, line_number, log_message, clean_errno());
+    } else {
 		if (status_code != RGAME_SUCCESS) {
 			fprintf(stderr, "%s (%s:%s:%d) %s (errno: %s).\n", lookup_status_code_string(status_code), 
-				file_name, function_name, line_number, msg, clean_errno());
-		}
-	#endif
+				file_name, function_name, line_number, log_message, clean_errno());
+		}		
+	}
 	
 	return status_code;
 }
