44e114ff4065a02b95fae0061c0567f36ef29c0c
Russell Coight <32256825+OutbackMan@users.noreply.github.com>
2018-02-26 14:00:32 +1100

    Update rgame.c

--- rgame.c
+++ rgame.c
@@ -1,23 +1,23 @@
 #include "rgame.h"
+#include "common.h"
 #include "status-codes.h"
 #include "debug.h"
 #include <stdbool.h>
 
-// __STDC_VERSION__ == 201112L
-rgame_status_t rgame_init(const char title[restrict static 1], int x_pos, int y_pos, int width, int height, bool want_fullscreen, struct RGame* restrict rgame)
+rgame_status_t rgame_init(const char title[restrict static 1], int x_pos, int y_pos, int width, int height, bool want_fullscreen, struct RGame* restrict rgame_instance)
 {
     // Check parameters
-    if (rgame == NULL) {
-        debug_error("Null argument passed to callee which requires a non-null argument");
+    if (rgame_instance == NULL) {
+        rgame_error("Null argument passed to callee which requires a non-null argument");
         return RGAME_ERROR;
     }
     
     if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
-        fprintf(stderr, "[RGAME_SDL_ERROR] %s\n", SDL_GetError());
+        rgame_sdl_error(SDL_GetError());
         return RGAME_SDL_ERROR;
     }
     
-    debug_log("SDL subsystems successfully initialised"); // [RGAME_LOG]
+    rgame_log("SDL subsystems successfully initialised");
     
     int window_flags = 0;
     
@@ -25,23 +25,23 @@ rgame_status_t rgame_init(const char title[restrict static 1], int x_pos, int y_
         window_flags = SDL_WINDOW_FULLSCREEN;
     }
     
-    rgame->window = SDL_CreateWindow(title, x_pos, y_pos, width, height, window_flags);
+    rgame_instance->window = SDL_CreateWindow(title, x_pos, y_pos, width, height, window_flags);
     
-    if (rgame->window == 0) {
+    if (rgame_instance->window == 0) {
         // Handle error
     }
     
-    debug_log("SDL window created");
+    rgame_log("SDL window created");
     
-    rgame->renderer = SDL_CreateRenderer(rgame->window, -1, 0);
+    rgame_instance->renderer = SDL_CreateRenderer(rgame->window, -1, 0);
     
-    if (rgame->renderer == 0) {
+    if (rgame_instance->renderer == 0) {
         // Handle error
     }
     
-    debug_log("SDL renderer created");
+    rgame_log("SDL renderer created");
     
-    rgame->is_running = true;
+    rgame_instance->is_running = true;
     
     return RGAME_SUCCESS;
 }
