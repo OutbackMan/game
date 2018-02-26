ddc6f3ef4b52f9aae8e5011b869e0ef49c10651f
Russell Coight <32256825+OutbackMan@users.noreply.github.com>
2018-02-26 15:03:18 +1100

    Update status-codes.h

--- status-codes.h
+++ status-codes.h
@@ -4,11 +4,14 @@
 #include <stddef.h>
 
 typedef enum status_codes {
-	RGAME_FAILURE,
+	RGAME_ERROR,
+	RGAME_NULL_ARGUMENT,
+	RGAME_SDL_ERROR,
 	RGAME_SUCCESS
 } rgame_status_t;
 
-static const size_t NUMBER_OF_STATUS_CODES = 2;
+static const size_t NUMBER_OF_STATUS_CODES = 4;
+static const size_t MAX_STATUS_CODE_VALUE = 3;
 
 const char* str_rgame_status(rgame_status_t status_code);
 
