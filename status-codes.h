7cfdbae34836e19f5e9667294263c5b9e2e23115
Russell Coight <32256825+OutbackMan@users.noreply.github.com>
2018-02-27 10:31:22 +1100

    Update status-codes.h

--- status-codes.h
+++ status-codes.h
@@ -24,16 +24,17 @@ static inline bool is_valid_status_code(rgame_status_t status_code)
 
 #if defined(WANT_LOGGING1)
 #define RGAME_STATUS(STATUS_CODE) \
-		rgame_status_log(1, STATUS_CODE, __FILE__, __func__, __LINE__)
-#elif defined(WANT_LOGGING2)
+		rgame_status_log1(STATUS_CODE, __FILE__, __func__, __LINE__)
+#elif defined(WANT_LOGGING2) || defined(WANT_LOGGING3)
 #define RGAME_STATUS(STATUS_CODE) \
-		rgame_status_log(2, STATUS_CODE, __FILE__, __func__, __LINE__)
-#elif defined(WANT_LOGGING3)
-#define RGAME_STATUS(STATUS_CODE) \
-		rgame_status_log(3, STATUS_CODE, __FILE__, __func__, __LINE__)
+		rgame_status_log_2_and_3(STATUS_CODE, __FILE__, __func__, __LINE__)
+#else
+#define RGAME_STATUS(STATUS_CODE) STATUS_CODE
 #endif
 
-const rgame_status_t rgame_status_log(const int log_level, const rgame_status_t status_code, const char file_location[restrict static 1], 
+const rgame_status_t rgame_status_log_1(const int log_level, const rgame_status_t status_code, const char file_location[restrict static 1], 
+							const char function_name[restrict static 1], const int line_number);
+const rgame_status_t rgame_status_log_2_and_3(const int log_level, const rgame_status_t status_code, const char file_location[restrict static 1], 
 							const char function_name[restrict static 1], const int line_number);
 
 const char* str_rgame_status(rgame_status_t status_code);
