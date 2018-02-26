2ef9554700f3a89b2586b164cc46648ea96f4219
Russell Coight <32256825+OutbackMan@users.noreply.github.com>
2018-02-26 15:16:50 +1100

    Update status-codes.h

--- status-codes.h
+++ status-codes.h
@@ -13,6 +13,9 @@ typedef enum status_codes {
 static const size_t NUMBER_OF_STATUS_CODES = 4;
 static const size_t MAX_STATUS_CODE_VALUE = 3;
 
+const rgame_status_t rgame_status(const rgame_status_t status_code, const char file_location[restrict static 1], 
+							const char function_name[restrict static 1], const int line_number);
+
 const char* str_rgame_status(rgame_status_t status_code);
 
 #endif
