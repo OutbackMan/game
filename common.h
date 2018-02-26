47e747749e536c83750d024b535b45140717352b
Russell Coight <32256825+OutbackMan@users.noreply.github.com>
2018-02-26 12:35:02 +1100

    Update common.h

--- common.h
+++ common.h
@@ -1,7 +1,15 @@
 #ifndef __common_h__
 #define __common_h__
 
-#define ARRAY_SIZE(array) (sizeof(array)/sizeof(array[0]))
+#if __STDC_VERSION__ == 201112L
+#define HAVE_C11
+#endif
+
+#ifndef HAVE_C11
+#error "RGame requires a compiler with C11 capabilities."
+#endif
+
+#define 1D_ARRAY_SIZE(array) (sizeof(array)/sizeof(array[0]))
 
 // min, max, abs, setbit, clearbit, togglebit, getmsb, getlsb
 
