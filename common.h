92ace9a2bef8693ac0ff89bfc53feae02b7971f6
Russell Coight <32256825+OutbackMan@users.noreply.github.com>
2018-02-28 10:37:43 +1100

    Update common.h

--- common.h
+++ common.h
@@ -1,7 +1,7 @@
 #ifndef __common_h__
 #define __common_h__
 
-#include <assert.h>
+#include <signal.h>
 #include <stdbool.h>
 
 #if __STDC_VERSION__ == 201112L
@@ -14,16 +14,33 @@
 
 #ifdef DEBUG
 #define IN_DEBUG_MODE
-#define WANT_LOGGING2
 #endif
 
+#ifdef IN_DEBUG_MODE
+#define RGAME_ASSERT(EXPR) \
+		do { \
+			if (!EXPR) \
+				rgame_assert(#EXPR, __FILE__, __func__, __LINE__) \
+		} while (0)
+#else
+#define RGAME_ASSERT(EXPR)
+#endif
+
+static inline void rgame_assert(const char expression[restrict static 1], const char file_name[restrict static 1],
+							const char function_name[restrict static 1], const int line_number)
+{
+	fprintf(stderr, "[RGAME_ASSERTION_ERROR] %s (%s:%s:%d)\n", expression, file_name, function_name, line_number);
+	raise(SIGABRT);
+}
+
+// MOVE INTO logging.h
 // Errors, successes and logs
 #ifdef LOGGING1
 #define WANT_LOGGING1
 #endif
 
 // Errors and logs
-#if defined(LOGGING2) && !defined(IN_DEBUG_MODE)
+#if defined(LOGGING2) || defined(IN_DEBUG_MODE)
 #define WANT_LOGGING2
 #endif
 
